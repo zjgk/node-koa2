fe2063e0b362ff66ec9af24e567197126ec1b187
zjgk <935299060@qq.com>
2019-09-01 22:23:24 +0800

    add login koa-session

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const bodyParser = require('koa-bodyparser');
 const mongoose = require('mongoose');
 const cors = require('koa2-cors')
 const app = new Koa();
+const Koa_Session = require('koa-session');
+
+// 配置session
+const session_signed_key = ["some secret hurr"];  // 这个是配合signed属性的签名key
+const session_config = {
+    key: 'koa:sess', /**  cookie的key。 (默认是 koa:sess) */
+    maxAge: 300,   /**  session 过期时间，以毫秒ms为单位计算 。*/
+    autoCommit: true, /** 自动提交到响应头。(默认是 true) */
+    overwrite: true, /** 是否允许重写 。(默认是 true) */
+    httpOnly: true, /** 是否设置HttpOnly，如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。  (默认 true) */
+    signed: true, /** 是否签名。(默认是 true) */
+    rolling: true, /** 是否每次响应时刷新Session的有效期。(默认是 false) */
+    renew: false, /** 是否在Session快过期时刷新Session的有效期。(默认是 false) */
+};
+const session = Koa_Session(session_config, app)
+app.keys = session_signed_key;
+// 使用中间件，注意有先后顺序
+app.use(session);
+
 app.use(bodyParser());  // 解析request的body
-// import router from './routes/index.js'
 const router = require('./routes/index.js');
 
 
@@ -21,29 +39,63 @@ app.use(cors({
 
 const db = mongoose.connect("mongodb://localhost/test")
 
-var UserSchema = new mongoose.Schema({
-    username:String,
-    password:String,
-	email:String,
-	name:String
-});
-var User = mongoose.model('user',UserSchema);
-const Cat = mongoose.model('Cat', { name: String });
+// var UserSchema = new mongoose.Schema({
+//     username:String,
+//     password:String,
+// 	email:String,
+// 	name:String
+// });
+// var User = mongoose.model('user',UserSchema);
+// const Cat = mongoose.model('Cat', { name: String });
+// router.get('/', async (ctx, next) => {
+// 	let val = null
+// 	const data = await User.findOne({name:'zhangnushi'})
+// 	const data2 = await Cat.findOne({name:'Zildjian'})
+// 	console.log('data', data,data2)
+// 	const result = {
+// 		code:200,
+// 		response: data2,
+// 		ts: 12345
+// 	}
+// 	ctx.response.body = result
+// 	return result
+// })
 
+app.use(async(ctx ,next)=> {
+    const databaseUserName = "testSession";
+    const databaseUserPasswd = "noDatabaseTest";
+    // 对/favicon.ico网站图标请求忽略
+    if (ctx.path === '/favicon.ico') return;
+    if (!ctx.session.logged) {  // 如果登录属性为undefined或者false，对应未登录和登录失败
+        // 设置登录属性为false
+        ctx.session.logged = false;
+        // 取请求url解析后的参数对象，方便比对
+        let query = ctx.request.query;
+        // 判断用户名密码是否为空
+        if (query.nickname && query.passwd) {
+            // 比对并分情况返回结果  
+            if (databaseUserName == query.nickname) {  // 如果存在该用户名
+                // 进行密码比对并返回结果 
+                ctx.body = (databaseUserPasswd == query.passwd) ? "登录成功" : "用户名或密码错误";
+                ctx.session.logged = true;
+            } else {                                                              //  如果用户名不存在
+				ctx.body = "用户名不存在";
+				console.log("用户名不存在")
+            }
+        } else {
+			ctx.body = "用户名密码不能为空";
+			console.log("用户名密码不能为空")
+			
+        }
+    } else {
+		ctx.body = "已登录";
+		console.log("已经登陆")
+		await next();
+    }
 
-router.get('/', async (ctx, next) => {
-	let val = null
-	const data = await User.findOne({name:'zhangnushi'})
-	const data2 = await Cat.findOne({name:'Zildjian'})
-	console.log('data', data,data2)
-	const result = {
-		code:200,
-		response: data2,
-		ts: 12345
-	}
-	ctx.response.body = result
-	return result
-})
+}
+);
+
 
 app.use(router.routes());
 
